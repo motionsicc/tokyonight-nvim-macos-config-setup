0286ba3dbede694632a03d7384ce9b620631c50d
Brian Ohaka <bohaka2016@gmail.com>
2026-03-05 19:38:52 -0500

    Current nvim macos setup updated configs

--- init.lua
+++ init.lua
@@ -6,13 +6,16 @@ if not vim.loop.fs_stat(lazypath) then
     "git",
     "clone",
     "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
+    "https://github.com", -- Fixed your URL here too
     "--branch=stable",
     lazypath,
   })
 end
 
 vim.opt.rtp:prepend(lazypath)
+
+-- Disable default virtual text so the new plugin can shine
+vim.diagnostic.config({ virtual_text = false })
 
 require("lazy").setup({
 
@@ -26,6 +29,15 @@ require("lazy").setup({
     end,
   },
 
+  -- THE NEW DIAGNOSTIC PLUGIN (Now in the right place!)
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    config = function()
+      require('tiny-inline-diagnostic').setup()
+    end
+  },
+
   -- Treesitter
   {
     "nvim-treesitter/nvim-treesitter",
@@ -35,14 +47,14 @@ require("lazy").setup({
       local ok, ts = pcall(require, "nvim-treesitter.configs")
       if ok then
         ts.setup({
-          ensure_installed = { "c", "cpp", "python" },
+          ensure_installed = { "c", "cpp", "python", "rust" },
           highlight = { enable = true },
         })
       end
     end,
   },
 
-      -- LSP
+  -- LSP
   {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -52,7 +64,7 @@ require("lazy").setup({
     config = function()
       require("mason").setup()
       require("mason-lspconfig").setup({
-        ensure_installed = { "clangd", "pyright" },
+        ensure_installed = { "clangd", "pyright", "rust_analyzer" },
         handlers = {
           function(server_name)
             local opts = {}
@@ -60,7 +72,7 @@ require("lazy").setup({
               opts = {
                 cmd = {
                   "clangd",
-                  "--query-driver=*", -- This tells clangd to search all system paths for your compiler
+                  "--query-driver=*", 
                   "--background-index",
                   "--clang-tidy",
                   "--header-insertion=never",
@@ -71,12 +83,11 @@ require("lazy").setup({
           end,
         },
       })
-      -- This shortcut lets you see the actual error message
+      -- Keymaps for help/errors
       vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = "Show line error" })
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "LSP Code Actions / Fix-it" })
     end,
   },
-
-
 
   -- Status line
   {
@@ -109,47 +120,48 @@ require("lazy").setup({
   },
 })
 
--- ===== Smart Build/Run Setup =====
+-- ===== Smart Build/Run Setup (MACOS OPTION KEY VERSION) =====
 local last_built_exe = ""
 
--- Smart Build (Ctrl+B)
-vim.keymap.set("n", "<C-b>", function()
-  vim.cmd("w") -- save file
+-- Smart Build (Option+B)
+vim.keymap.set("n", "<M-b>", function()
+  vim.cmd("w") 
   local ft = vim.bo.filetype
-  local file = vim.fn.expand("%:p"):gsub("\\", "/")
-  last_built_exe = vim.fn.expand("%:p:r"):gsub("\\", "/") .. ".exe"
+  local file = vim.fn.expand("%:p")
+  last_built_exe = vim.fn.expand("%:p:r")
 
   if ft == "cpp" then
     vim.cmd("botright split | resize 12")
-    -- Compiling C++ with g++
     vim.cmd(string.format("terminal g++ -std=c++23 -Wall -Wextra \"%s\" -o \"%s\"", file, last_built_exe))
   elseif ft == "c" then
     vim.cmd("botright split | resize 12")
-    -- Compiling pure C with gcc to avoid C++ warnings
     vim.cmd(string.format("terminal gcc -std=c17 -Wall -Wextra \"%s\" -o \"%s\"", file, last_built_exe))
+  elseif ft == "rust" then
+    vim.cmd("botright split | resize 12")
+    vim.cmd(string.format("terminal rustc \"%s\" -o \"%s\"", file, last_built_exe))
   elseif ft == "python" then
-    print("Python is interpreted; no build needed! Use Ctrl+R to run.")
+    print("Python is interpreted; no build needed! Use Option+R to run.")
   else
     print("Build not configured for filetype: " .. ft)
   end
 end, { noremap = true, silent = true })
 
--- Smart Run (Ctrl+R)
-vim.keymap.set("n", "<C-r>", function()
+-- Smart Run (Option+R)
+vim.keymap.set("n", "<M-r>", function()
   local ft = vim.bo.filetype
-  local file = vim.fn.expand("%:p"):gsub("\\", "/")
+  local file = vim.fn.expand("%:p")
 
-  if ft == "cpp" or ft == "c" then
-    if last_built_exe == "" then last_built_exe = vim.fn.expand("%:p:r"):gsub("\\", "/") .. ".exe" end
+  if ft == "cpp" or ft == "c" or ft == "rust" then
+    if last_built_exe == "" then last_built_exe = vim.fn.expand("%:p:r") end
     if vim.fn.filereadable(last_built_exe) == 1 then
       vim.cmd("botright split | resize 12")
-      vim.cmd("terminal \"" .. last_built_exe .. "\"")
+      vim.cmd("terminal ./" .. vim.fn.fnamemodify(last_built_exe, ":t"))
     else
-      print("Error: No executable found. Build first with Ctrl+B.")
+      print("Error: No executable found. Build first with Option+B.")
     end
   elseif ft == "python" then
     vim.cmd("botright split | resize 12")
-    vim.cmd("terminal python \"" .. file .. "\"")
+    vim.cmd("terminal python3 \"" .. file .. "\"")
   else
     print("Run not configured for filetype: " .. ft)
   end
@@ -158,8 +170,13 @@ end, { noremap = true, silent = true })
 -- Open Mason with <leader>m
 vim.keymap.set("n", "<leader>m", ":Mason<CR>", { noremap = true, silent = true })
 
--- Force close terminal with Ctrl-X (No Esc needed)
-vim.keymap.set('t', '<C-x>', [[<C-\><C-n>:q<CR>]], { noremap = true, silent = true })
+-- Force close terminal with Option-X
+vim.keymap.set('t', '<M-x>', [[<C-\><C-n>:q<CR>]], { noremap = true, silent = true })
 
-vim.opt.number = true         -- Enables the line numbers
-vim.opt.relativenumber = false -- Ensures numbers aren't "reversed" (relative)
+-- Global Options
+vim.opt.number = true         
+vim.opt.relativenumber = false 
+vim.opt.tabstop = 4      
+vim.opt.softtabstop = 4  
+vim.opt.shiftwidth = 4   
+vim.opt.expandtab = true 
